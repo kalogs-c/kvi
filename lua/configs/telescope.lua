656ef850b52731f424d4176b3b0de9183e052d83
Carlos Camilo <carloscamilocontato@gmail.com>
2024-08-10 17:08:08 -0300

    ignore elixir folders telescope fix

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,7 +1,3 @@
 local config = require "nvchad.configs.telescope"
 
--- elixir ignore
-table.insert(config.defaults.file_ignore_patterns, "_build")
-table.insert(config.defaults.file_ignore_patterns, "deps")
-
 return config
