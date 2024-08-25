599b8616b4acb997a57c5004e3d56450bac2db81
Carlos Camilo <carloscamilocontato@gmail.com>
2024-08-25 17:45:48 -0300

    git things

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
 
-require "mappings.neotest"
-require "mappings.trouble"
+-- load all mappings from lua/mappings/*.lua
+local mappings_path = vim.fn.stdpath "config" .. "/lua/mappings"
+local files = vim.fn.readdir(mappings_path, [[v:val =~ '\.lua$']])
+for _, file in ipairs(files) do
+  require("mappings." .. file:gsub("%.lua", ""))
+end
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "User FilePost",
+  opts = function()
+    local options = require "nvchad.configs.gitsigns"
+    options.current_line_blame = true
+    return options
+  end,
+}
