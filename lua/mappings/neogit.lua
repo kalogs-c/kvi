142aa405765b979bb86b86c11963b7dfcfb5c2f2
Carlos Camilo <carloscamilocontato@gmail.com>
2024-08-25 17:50:48 -0300

    neogit mais

--- lua/mappings/neogit.lua
+++ lua/mappings/neogit.lua
@@ -3,6 +3,15 @@ local map = vim.keymap.set
 local neogit = require "neogit"
 
 map("n", "<leader>gs", neogit.open, { desc = "Git - Neogit - Open summary" })
+
 map("n", "<leader>gd", function()
   neogit.open { "diff" }
 end, { desc = "Git - Neogit - Open diff" })
+
+map("n", "<leader>gc", function()
+  neogit.open { "commit" }
+end, { desc = "Git - Neogit - Commit" })
+
+map("n", "<leader>gp", function()
+  neogit.open { "push" }
+end, { desc = "Git - Neogit - Push" })
