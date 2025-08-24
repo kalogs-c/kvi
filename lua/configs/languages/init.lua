89900930b20a725f56a38a5f958d19c711b566a3
brendhacasaro <brendhameira9@gmail.com>
2025-08-23 21:11:33 -0300

    java

--- lua/configs/languages/init.lua
+++ lua/configs/languages/init.lua
@@ -1,28 +1,33 @@
 return {
-  lua = {
-    lsp = "lua_ls",
-    tools = "stylua",
-    fmt = "stylua",
-  },
-  go = {
-    lsp = "gopls",
-    tools = { "delve", "goimports", "gofumpt" },
-    fmt = { "goimports", "gofumpt" },
-  },
+	lua = {
+		lsp = "lua_ls",
+		tools = "stylua",
+		fmt = "stylua",
+	},
+	go = {
+		lsp = "gopls",
+		tools = { "delve", "goimports", "gofumpt" },
+		fmt = { "goimports", "gofumpt" },
+	},
 
-  javascript = {
-    fmt = "prettier",
-    lsp = "ts_ls"
-  },
+	javascript = {
+		fmt = "prettier",
+		lsp = "ts_ls",
+	},
 
-  html = {
-    fmt = "prettier",
-    lsp = "html",
-    tools = "html-lsp"
-  },
+	html = {
+		fmt = "prettier",
+		lsp = "html",
+		tools = "html-lsp",
+	},
 
-  css = {
-    fmt = "prettier",
-    lsp = "cssls"
-  }
+	css = {
+		fmt = "prettier",
+		lsp = "cssls",
+	},
+
+	java = {
+		fmt = "google-java-format",
+		lsp = "jdtls",
+	},
 }
